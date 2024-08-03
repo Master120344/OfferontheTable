832bb777ab4fb9bf3748993fa9de361167c5f166
JT <Master120344@yahoo.com>
2024-08-02 21:08:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     $('.menu-toggle').click(function() {
         $('nav ul').toggleClass('showing');
     });
+
+    // Smooth scrolling for links
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 });
