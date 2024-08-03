904f84fb929e9295019898182025df6b82573fcf
JT <Master120344@yahoo.com>
2024-08-02 21:02:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Add any JavaScript for dynamic features here
-
-$(document).ready(function(){
-    $('nav ul li a').click(function(){
-        $('nav ul li a').removeClass('active');
-        $(this).addClass('active');
+$(document).ready(function() {
+    $('.menu-toggle').click(function() {
+        $('nav ul').toggleClass('showing');
     });
 });
